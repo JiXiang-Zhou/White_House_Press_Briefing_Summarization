43c8cdd279184dc9722f46f0688ba8e634a094b9
JiXiang-Zhou <84003791+JiXiang-Zhou@users.noreply.github.com>
2021-06-02 16:00:58 +0800

    Add files via upload

---
+++ Data_Split.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import bs4 as bs
+import urllib.request
+import re
+import urllib.request
+
+
+def get_QaA(link):
+    scraped_data = urllib.request.urlopen(link)
+    article = scraped_data.read()
+
+    parsed_article = bs.BeautifulSoup(article,'lxml')
+
+    paragraphs = parsed_article.find_all('p')
+
+    article_text = ""
+
+    for p in paragraphs:
+        article_text += p.text
+
+    article_Cluster = re.split('\xa0Q\xa0',article_text)
+    
+    QaA = []
+    for cluster in article_Cluster:
+        QaA.append(re.split('[A-Z][A-Z][A-Z]+:',cluster,maxsplit=1))
+        
+    for response in QaA:   
+        if len(response)==1:
+            QaA.remove(response)
+            
+    Question = []
+    for Qs in QaA:
+        Question.append(re.sub('\xa0',' ',Qs[0]))
+    Question[0]='openning'
+    
+    Answer = []
+    for As in QaA:
+        Answer.append(re.sub('\xa0',' ',As[1]))
+    
+    QaAs = list(zip(Question,Answer))
+    
+    return QaAs
+
+
+# In[7]:
+
+
+
+
+
+# In[8]:
+
+
+
+
+
+# In[9]:
+
+
+
+
+
+# In[10]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
