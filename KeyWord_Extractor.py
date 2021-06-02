25226f7eb3777107dffb82ec2fa4af8f8cd3a5c9
JiXiang-Zhou <84003791+JiXiang-Zhou@users.noreply.github.com>
2021-06-02 14:19:13 +0800

    Add files via upload

---
+++ KeyWord_Extractor.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from keybert import KeyBERT
+from wordwise import Extractor
+
+
+# In[5]:
+
+
+def length_count(sentence):
+    a = sentence.split() 
+    num = len(a)
+    return num
+
+
+# In[40]:
+
+
+def press_briefing_extractor_short(doc):
+    
+    extractor = Extractor()
+    keywords = extractor.generate(doc, 5)
+    
+    model = KeyBERT('distilbert-base-nli-mean-tokens')
+    scores=model.extract_keywords(doc, keyphrase_ngram_range=(1, 2))
+    
+    for key in scores:
+        keywords.append(key[0]) 
+    
+    keywords = list(set(keywords))
+    return keywords
+
+
+# In[27]:
+
+
+def words_to_sentences(words):
+    length = len(words)
+    sentence=''
+    for i in range(0,length):
+        sentence = sentence+' '+words[i]
+    return sentence
+
+
+# In[48]:
+
+
+def long_paragraph_split(paragraph,length = 400):
+    words = paragraph.split()
+    words_length = len(words)
+    i = 0
+    splited_paragraph=[]
+    while(i<words_length):
+        splited_paragraph.append(words_to_sentences(words[i:i+length]))
+        i=i+length
+    return splited_paragraph
+
+
+# In[51]:
+
+
+def press_briefing_extractor(doc ,length_limit = 400):
+    if length_count(doc)<=length_limit:
+        keywords = press_briefing_extractor_short(doc)
+    else:
+        splited_doc = long_paragraph_split(doc, length_limit)
+        keywords = []
+        for splited_sentence in splited_doc:
+            keywords = keywords + press_briefing_extractor_short(splited_sentence)
+            keywords = list(set(keywords))
+            
+    return keywords
+
+
+def filterout_short_QaA(QaAs, Q_min = 50, A_min = 22):
+    
+    filtered_QaAs = []
+    
+    for QaA in QaAs:
+        A=True
+        Q=True
+        
+        if length_count(QaA[1]) < Q_min:
+            A=False
+        elif  length_count(QaA[0]) < A_min:
+            Q=False
+        
+        if A:
+            if Q:
+                filtered_QaAs.append(QaA)
+            else:
+                tagging = [QaA[1],QaA[1]]
+                filtered_QaAs.append(tagging)
+    
+    return filtered_QaAs
+# In[ ]:
+
+
+
+
